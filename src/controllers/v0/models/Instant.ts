e438440c776ea1373162bbebde9ba8a165f7bab9
Mohammed Tantawy <mtantawy.mido@gmail.com>
2020-05-11 20:41:30 +0200

    Added Instant Model

---
+++ src/controllers/v0/models/Instant.ts
@@ -0,0 +1,49 @@
+import { Document, model, Schema } from "mongoose"
+
+const InstantSchema: Schema = Schema({
+    _id: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    photoname: {
+        type: String,
+        required: true
+    },
+    weight: {
+        type: String,
+        required: true
+    },
+    length: {
+        type: String,
+        required: true
+    },
+    latitude: {
+        type: String,
+        required: true
+    },
+    longitude: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: String,
+        required: true
+    }
+})
+
+export interface IInstant extends Document {
+    _id: string,
+    username: string,
+    photoname: string,
+    weight: string,
+    length: string,
+    latitude: string,
+    longitude: string,
+    timestamp: string
+}
+
+export default model('Instant', InstantSchema);
