43ddc32a5c98965b7b843a209569eced39ca465a
Mohammed Tantawy <mtantawy.mido@gmail.com>
2020-05-18 01:05:36 +0200

    Added test for creating an instant

--- src/controllers/v0/instant/routes/instant.router.spec.ts
+++ src/controllers/v0/instant/routes/instant.router.spec.ts
@@ -1,5 +1,7 @@
 import frisby from 'frisby'
-import { ENDPOINT_GET_ALL, ENDPOINT_SENDRESIZE_TOQUEUE } from '../../../../utils/constants'
+import path from 'path'
+import fs from 'fs'
+import { ENDPOINT_GET_ALL, ENDPOINT_CREATE_INSTANT, ENDPOINT_SENDRESIZE_TOQUEUE } from '../../../../utils/constants'
 
 /**
  * This test gets elements from the DB from the /all endpoint
@@ -8,6 +10,27 @@ test('Should get elements from DB', () => {
     return frisby
         .get(ENDPOINT_GET_ALL)
         .expect('status', 200);
+})
+
+/**
+ * Test insertion of instant in the DB. It takes the image from /public/test
+ */
+test('Should insert instant in the DB successfully', () => {
+    const imgPath = path.resolve('public/test/mock-img.png');
+    let content: any = fs.createReadStream(imgPath);
+    let formData = frisby.formData();
+    formData.append("username", "mido")
+    formData.append("photoname", "space")
+    formData.append("weight", "313kb")
+    formData.append("length", "long")
+    formData.append("latitude", "12s 44n")
+    formData.append("longitude", "33e 13w")
+    formData.append('details', content);
+
+    return frisby
+        .post(ENDPOINT_CREATE_INSTANT, { body: formData })
+        .inspectRequestHeaders()
+        .expect('status', 200)
 })
 
 /**
